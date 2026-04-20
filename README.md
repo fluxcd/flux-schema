bbb6053dd2d8e4f90c42d293f9af567188affa1d
Stefan Prodan <stefan.prodan@gmail.com>
2026-04-20 11:37:18 +0300

    Init Flux CLI plugin
    Signed-off-by: Stefan Prodan <stefan.prodan@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # flux-schema
-Flux CLI plugin for Kubernetes schema extraction and manifests validation
+
+[![test](https://github.com/fluxcd/flux-schema/actions/workflows/test.yaml/badge.svg)](https://github.com/fluxcd/flux-schema/actions/workflows/test.yaml)
+[![license](https://img.shields.io/github/license/fluxcd/flux-schema.svg)](https://github.com/fluxcd/flux-schema/blob/main/LICENSE)
+
+Flux CLI plugin for Kubernetes schema extraction and manifests validation.
+
+> [!NOTE]
+> This repository is in early development and the plugin system is not yet available in a stable release of Flux.
+> The instructions for installing and using the plugin will be added here when [RFC-0013](https://github.com/fluxcd/flux2/blob/main/rfcs/0013-cli-plugin-system/README.md)
+> has been implemented and released in Flux 2.9 or later.
---
+++ cmd/flux-schema/main.go
@@ -0,0 +1,49 @@
+// Copyright 2026 The Flux Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	"os"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	VERSION = "0.0.0-dev.0"
+)
+
+var rootCmd = &cobra.Command{
+	Use:               "flux-schema",
+	Version:           VERSION,
+	SilenceUsage:      true,
+	SilenceErrors:     true,
+	DisableAutoGenTag: true,
+	Long: `Flux CLI plugin for Kubernetes schema extraction and manifests validation.
+⚠️ Please note that this plugin is in preview and under development.
+While we try our best to not introduce breaking changes, they may occur when
+we adapt to new features and/or find better ways to facilitate what it does.`,
+}
+
+type rootFlags struct {
+	timeout time.Duration
+}
+
+var rootArgs = rootFlags{
+	timeout: time.Minute,
+}
+
+func init() {
+	rootCmd.PersistentFlags().DurationVar(&rootArgs.timeout, "timeout", rootArgs.timeout,
+		"The length of time to wait before giving up on the current operation.")
+
+	rootCmd.SetOut(os.Stdout)
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		rootCmd.PrintErrf("✗ %v\n", err)
+		os.Exit(1)
+	}
+}
